5eac86f4f09e1392ca8288e10add660bd1fa62ba
yoshixmk <pirori67@gmail.com>
2021-08-17 07:52:48 +0900

    upgrade versions

---
+++ README.md
@@ -0,0 +1,22 @@
+# News API sample project
+
+## Usage
+
+Get api key at [here](https://newsapi.org/).
+
+```bash
+$ yarn
+$ yarn dev
+```
+
+Or, 
+```
+API_KEY=XXX yarn dev
+```
+
+## Formater and Linter
+
+```bash
+$ yarn check
+$ yarn fix
+```
--- src/index.ts
+++ src/index.ts
@@ -6,53 +6,66 @@ const newsapi = new NewsAPI(process.env.API_KEY);
 // To query top headlines
 // All options passed to topHeadlines are optional, but you need to include at least one of them
 newsapi.v2.topHeadlines({
-	q: "trump",
-	category: "politics",
-	language: "en",
-	country: "us",
+  q: "trump",
+  category: "politics",
+  language: "en",
+  country: "us",
 }).then((response: Response) => {
-	console.log(response);
-	/*
+  console.log(response);
+  /*
 {
   status: "ok",
   articles: [...]
 }
-*/
+  */
 });
 
-// To query everything
-// You must include at least one q, source, or domain
+function formatDate(dt: Date) {
+  var y = dt.getFullYear();
+  var m = ("00" + (dt.getMonth() + 1)).slice(-2);
+  var d = ("00" + dt.getDate()).slice(-2);
+  return (y + "-" + m + "-" + d);
+}
+
+function getBeforeDays(dt: Date, n: number) {
+  dt.setDate(dt.getDate() - n);
+  return dt;
+}
+
+const from = formatDate(getBeforeDays(new Date(), 30));
+const to = formatDate(new Date());
+
 newsapi.v2.everything({
-	q: "trump",
-	sources: "bbc-news,the-verge",
-	domains: "bbc.co.uk,techcrunch.com",
-	from: "2020-07-24",
-	to: "2020-08-24",
-	language: "en",
-	sortBy: "relevancy",
-	page: 2,
+  q: "trump",
+  sources: "bbc-news,the-verge",
+  domains: "bbc.co.uk,techcrunch.com",
+  from, //
+  to,
+  language: "en",
+  sortBy: "relevancy",
+  page: 2,
 }).then((response: Response) => {
-	console.log(response);
-	/*
+  console.log(response);
+  /*
 {
   status: "ok",
   articles: [...]
 }
-*/
+  */
 });
 
 // To query sources
 // All options are optional
 newsapi.v2.sources({
-	category: "technology",
-	language: "en",
-	country: "us",
+  category: "technology",
+  language: "en",
+  country: "us",
 }).then((response: Response) => {
-	console.log(response);
-	/*
+  console.log(response);
+  /*
 {
   status: "ok",
   sources: [...]
 }
-*/
+  */
 });
